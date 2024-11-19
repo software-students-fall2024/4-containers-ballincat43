293db9edf89c41073b3e40556fd7c345c06b5b63
drj8812 <drj8812@nyu.edu>
2024-11-19 17:30:00 -0500

    fixing some formatting

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -1,21 +1,20 @@
-'''
+"""
 This file contains the functions to perform some
 analysis on data recieved.
-'''
+"""
 
-#starting some functions:
-
+# starting some functions:
 
 
 def vocab_diversity(data: str) -> tuple:
-    '''
-    This function takes in a string and calculates the frequency of 
+    """
+    This function takes in a string and calculates the frequency of
     unique words to total words as well as the most common word. (Words must be
     alphabetic). It returns a tuple of length 2, with the first item being the
     most common word in the given string and the second being the frequency. If
-    the string is empty 
-    '''
-    #split into words
+    the string is empty.
+    """
+    # split into words
     words = data.split()
     di = {}
     total: int = 0
@@ -24,30 +23,31 @@ def vocab_diversity(data: str) -> tuple:
         w = w.lower()
         if not str.isalpha(w):
             continue
-        total+=1
+        total += 1
         if w in di:
-            di[w]+=1
+            di[w] += 1
         else:
             di[w] = 1
-            unique+=1 #could also count len(keys) but for performace
+            unique += 1 # could also count len(keys) but for performace
     if total == 0:
-        freq = 1 #if no words technically all unique
+        freq = 1 # if no words technically all unique
     else:
         freq: float =  unique / total
     return most_common_dict(di), round(freq, 2)
 
+
 def most_common_dict(word_freq: dict, every: bool = False):
-    '''
-    Calculates the key mapped to the highest value, if multiple keys 
-    map to the highest values, then the lowest one lexicographically 
-    is returned, unless the 'every' flag is set to true, in which case 
+    """
+    Calculates the key mapped to the highest value, if multiple keys
+    map to the highest values, then the lowest one lexicographically
+    is returned, unless the 'every' flag is set to true, in which case
     all of them are returned as a list.
-    '''
+    """
     m = 0
     if every:
         ms = []
     else:
-        ms = "" #max string
+        ms = "" # max string
     for k in sorted(word_freq.keys()):
         v = word_freq.get(k)
         if v > m:
@@ -56,5 +56,4 @@ def most_common_dict(word_freq: dict, every: bool = False):
                 ms.append(k)
             else:
                 ms = k
-    return ms
-    
+    return ms    
--- machine-learning-client/test_diversity.py
+++ machine-learning-client/test_diversity.py
@@ -1,42 +1,46 @@
-'''
+"""
 This file contains tests for the vocab_diversity() function.
-'''
+"""
 from main import vocab_diversity
 
+
 def test_unique():
-    '''
+    """
     Tests that a string of unique words returns
     a frequency of 1 and the first word.
-    '''
+    """
     s = "apple blue car drive eagle flame green house"
     m, f = vocab_diversity(s)
     assert f == 1
     assert m == "apple"
 
+
 def test_repeat():
-    '''
+    """
     Tests that a string of the same repeated word gives
     a frequency of 1/total and the correct common word.
-    '''
+    """
     s = "apple apple apple apple apple"
     m, f = vocab_diversity(s)
-    assert f == .2
+    assert f == 0.2
     assert m == "apple"
 
+
 def test_none():
-    '''
+    """
     Tests the functions behavior with an empty string.
-    '''
+    """
     s = ""
     m, f = vocab_diversity(s)
     assert f == 1
     assert m == ""
 
+
 def test_reg():
-    '''
+    """
     Tests the function on a more "normal" input.
-    '''
+    """
     s = "That coffee is so hot that it burned the paper"
     m, f = vocab_diversity(s)
-    assert f == .9
+    assert f == 0.9
     assert m == "that"
