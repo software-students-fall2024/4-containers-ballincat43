6a0cc4305227fc96936b1d81caa9ad4553ee0598
drj8812 <drj8812@nyu.edu>
2024-11-21 12:28:06 -0500

    fixed linting

--- web-app/test_app.py
+++ web-app/test_app.py
@@ -2,49 +2,43 @@
 This tests the app.py file.
 """
 
-import pytest
-from app import app, User
 from flask_login import FlaskLoginClient
+from app import app, User
 
-@pytest.fixture
-def appmake():
-    """app fixture to test"""
-    app.secret_key = "tripledoubleholymoly"  # set secret key
-    app.test_client_class = FlaskLoginClient
 
-def test_index(appmake):
+def test_index():
     """tests index redirects"""
     with app.test_client() as client:
         assert client.get("/").status_code == 302  # since immediately redirects
 
 
-def test_login(appmake):
+def test_login():
     """test that login works"""
     with app.test_client() as client:
         assert client.get("/login").status_code == 200
 
 
-def test_create_account(appmake):
+def test_create_account():
     """testing create account"""
     with app.test_client() as client:
         assert client.get("/create_account").status_code == 200
 
 
-def test_logout(appmake):
+def test_logout():
     """testing logout"""
     user = User("bob123")
     with app.test_client(user=user) as client:
         assert client.get("/logout").status_code == 200
 
 
-def test_username(appmake):
+def test_username():
     """Testing username"""
     user = User("bob123")
     with app.test_client(user=user) as client:
         assert client.get("/bob123").status_code == 200
 
 
-def test_stats(appmake):
+def test_stats():
     """testing stats"""
     user = User("bob123")
     with app.test_client(user=user) as client:
